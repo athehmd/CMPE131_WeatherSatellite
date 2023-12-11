669a41ae9e9dd3282e5a60381165748fec008e32
athehmd <36544534+athehmd@users.noreply.github.com>
2023-12-10 17:18:14 -0800

    Add files via upload

--- create_db.sql
+++ create_db.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE 'weather_satellite';
+CREATE DATABASE `weather_satellite`;
+
+USE `weather_satellite`;
 
 CREATE TABLE `Data` (
-	`Data` INT NULL,
-	`Date` DATETIME NOT NULL,
-	`Author` VARCHAR(50) NOT NULL DEFAULT ''
+    `Data` INT NULL,
+    `Date` DATETIME NOT NULL,
+    `Author` VARCHAR(50) NOT NULL DEFAULT ''
 );
-
